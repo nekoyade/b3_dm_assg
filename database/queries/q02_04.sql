ea5b214bec41338fd0cb98018d8e649b161e3b0e
nekoyade <ktr.km.sch@gmail.com>
2024-06-28 20:30:05 +0900

    Modify some queries

--- database/queries/q02_04.sql
+++ database/queries/q02_04.sql
@@ -2,6 +2,8 @@ SELECT DISTINCT
         p.date, p.time_slot, g.name group_name, v.name venue, s.name section
     FROM performances p
     JOIN groups g ON p.group_id = g.id
+    JOIN persons_groups j ON g.id = j.group_id
+    JOIN persons r ON j.person_id = r.id
     JOIN sections s ON p.section_id = s.id
     JOIN venues v ON s.venue_id = v.id
-    WHERE p.date like '2024-%' and s.name like '%ホール';
+    WHERE r.name = '零零 七五';
